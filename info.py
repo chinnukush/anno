80c67be9ec55425e83b48960bff41ec41aa5681c
chinnukush <chinnukushal123@gmail.com>
2025-05-25 19:29:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), False) # Type Tr
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://anikush8310:hr62FDk6kCMlFpZv@cluster0.wrau1j0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+_kU_e_VpRp80Nzc1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+63JiMC4lCjBjZDFl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+W6H8sCxri9MzYzQ1')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/TG_BOTS_UPDATE')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/TG_Bots_Support')
 

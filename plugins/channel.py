29748ed2928229a0583fb27ebceff18ec01e3517
chinnukush <chinnukushal123@gmail.com>
2025-08-30 09:39:57 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -60,7 +60,7 @@ async def send_movie_update(bot, file_name, caption):
         unique_id = generate_unique_id(search_movie)
         reaction_counts[unique_id] = {"❤️": 0, "👍": 0, "👎": 0, "🔥": 0}
         user_reactions[unique_id] = {}
-        caption_template = "<b>#Added ✅\n\nName: {}\nQuality: {}\nAudio: {}</b>"
+        caption_template = "<b>🍿 ᴛɪᴛʟᴇ: {}\n👀 ǫᴜᴀʟɪᴛʏ: {}\n🎵 ᴀᴜᴅɪᴏ: {}</b>"
         full_caption = caption_template.format(file_name, quality, language)
         if kind:
             full_caption += f"\n<b>#{kind}</b>"
